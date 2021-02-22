437741097e9b6ff02e0b660f78c5dc46f821c756
nynaalekhya <50094420+nynaalekhya@users.noreply.github.com>
2021-02-22 03:26:37 -0800

    Add files via upload

---
+++ client/core/Menu.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import IconButton from '@material-ui/core/IconButton'
+import HomeIcon from '@material-ui/icons/Home'
+import Button from '@material-ui/core/Button'
+import auth from './../auth/auth-helper'
+import {Link, withRouter} from 'react-router-dom'
+
+const isActive = (history, path) => {
+  if (history.location.pathname == path)
+    return {color: 'black'}
+  else
+    return {color: '#ffffff'}
+}
+const Menu = withRouter(({history}) => (
+  <AppBar position="static">
+    <Toolbar>
+      <Link to="/home">
+      <Typography variant="h5" color="inherit" style={{color: '#ffffff', margin: '0 10px'}}>
+       ChatRoom 
+      </Typography>
+      </Link>
+      <Link to="/home">
+        <IconButton aria-label="Home" style={isActive(history, "/home")}>
+          <HomeIcon/>
+        </IconButton>
+      </Link>
+      {
+        !auth.isAuthenticated() && (<span>
+          <Link to="/signup">
+            <Button style={isActive(history, "/signup")}>Sign up
+            </Button>
+          </Link>
+          <Link to="/signin">
+            <Button style={isActive(history, "/signin")}>Sign In
+            </Button>
+          </Link>
+        </span>)
+      }
+      {
+        auth.isAuthenticated() && (<span>
+          <Link to={"/user/" + auth.isAuthenticated().user._id}>
+            <Button style={isActive(history, "/user/" + auth.isAuthenticated().user._id)}>My Profile</Button>
+          </Link>
+          <Button color="inherit" onClick={() => {
+              auth.clearJWT(() => history.push('/'))
+            }}>Sign out</Button>
+        </span>)
+      }
+    </Toolbar>
+  </AppBar>
+))
+
+export default Menu
